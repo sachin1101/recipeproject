6496508b58a2a970600eeed6f593f17fc0830728
user01 <sachin.saraswat1741@gmail.com>
2019-01-02 20:24:12 -0500

    Commit #02

---
+++ src/main/java/com/springframework/recipeproject/controllers/RecipeHomeController.java
@@ -0,0 +1,18 @@
+package com.springframework.recipeproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class RecipeHomeController {
+
+    @RequestMapping({"", "/","/index"})
+    public String home()
+    {
+        System.out.println("122343536");
+
+        return "Index";
+    }
+
+}
