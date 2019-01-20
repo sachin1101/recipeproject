eb94a7503da03135eba2f2f0bbe307eb3aa0382f
user01 <sachin.saraswat1741@gmail.com>
2019-01-19 21:11:35 -0500

    Commit #02

--- src/main/java/com/springframework/recipeproject/controllers/RecipeHomeController.java
+++ src/main/java/com/springframework/recipeproject/controllers/RecipeHomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class RecipeHomeController {
 
-    @RequestMapping({"", "/","/index"})
+    @RequestMapping({"", "/","/index", "/test"})
     public String home()
     {
         System.out.println("122343536");
